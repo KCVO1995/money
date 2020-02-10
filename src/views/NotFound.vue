b446b7d27bccf58c5760910335962818f7b33b2c
KCVO1995 <943514589@qq.com>
2020-02-10 18:25:32 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>对不起，您要找的页面被小呆丢失了</div>
+    <div><router-link to="/">返回首页吧，乖！</router-link> </div>
+  </div>
+</template>
+
+<script lang='ts'>
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang='scss' scoped>
+
+</style>
