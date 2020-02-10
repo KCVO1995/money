89c70032a4e23f7fe4da40846d296b81c2ad9c39
KCVO1995 <943514589@qq.com>
2020-02-10 19:45:15 +0800

    布局 money 页面

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    I am money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      I am money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@
 </script>
 
 <style lang='scss' scoped>
+ .nav-wrapper {
+   border: 1px solid green;
+   display: flex;
+   flex-direction: column;
+   height: 100vh;
+ }
+  .content {
+    flex-grow: 1;
+    overflow: auto;
+  }
 
 </style>
