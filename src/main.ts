bac36b2ac24224ee3bb7e9d6618feb8b6c1be546
KCVO1995 <943514589@qq.com>
2020-02-10 17:24:22 +0800

    创建三个页面

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router'; // js的潜规则，如果路径是个目录，自动找里面的index
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
+  router, // 引入路由
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
