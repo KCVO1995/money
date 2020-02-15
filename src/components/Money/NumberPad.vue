e81b98635cb407edf96eb9965853ed1a58f6d94c
KCVO1995 <943514589@qq.com>
2020-02-15 17:33:34 +0800

    基本完成 numberPad 组件功能

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,32 +1,69 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{output}}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button @click="ok" class="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
+
 <script lang='ts'>
-  export default {
-    name: 'NumberPad'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NumberPad extends Vue {
+    output = '0';
+
+    inputContent(event: MouseEvent) {
+      const button = event.target as HTMLButtonElement;
+      const input = button.textContent as string;
+
+      // 最大支持16位数字 使用0-9替代初始0,并排除 00 .. 的情况
+      if (this.output.length < 16) {
+        if (this.output === '0' && input !== '.') {
+          this.output = input;
+        } else if (input !== '.' || input === '.' && this.output.indexOf('.') === -1) {
+          this.output += input;
+        }
+      }
+    }
+
+    remove() {
+      if (this.output.length > 1) {
+        this.output = this.output.slice(0, -1);
+      } else {
+        this.output = '0';
+      }
+    }
+
+    clear() {
+      this.output = '0';
+    }
+
+    ok() {
+      this.output = '0';
+      console.log('ok');
+    }
+  }
 </script>
 
 <style lang='scss' scoped>
   @import "~@/assets/style/helper.scss";
+
   .numberPad {
     > .output {
       @extend %innerShadow;
@@ -35,40 +72,52 @@
       padding: 9px 16px;
       text-align: right;
     }
+
     > .buttons {
       @extend %clearFix;
+
       > button {
         float: left;
         height: 64px;
         width: 25%;
         border: none;
         background: transparent;
+
         &.ok {
           height: 128px;
           float: right;
         }
+
         &.zero {
           width: 50%;
         }
+
         $bgc: #f2f2f2;
+
         &:nth-child(1) {
           background: $bgc;
         }
+
         &:nth-child(2), &:nth-child(5) {
           background: darken($bgc, 4%);
         }
+
         &:nth-child(3), &:nth-child(6), &:nth-child(9) {
           background: darken($bgc, 4*2%);
         }
+
         &:nth-child(4), &:nth-child(7), &:nth-child(10) {
           background: darken($bgc, 4*3%);
         }
+
         &:nth-child(8), &:nth-child(11), &:nth-child(13) {
           background: darken($bgc, 4*4%);
         }
+
         &:nth-child(14) {
           background: darken($bgc, 4*5%);
         }
+
         &:nth-child(12) {
           background: darken($bgc, 4*6%);
         }
