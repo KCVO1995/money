3f7656996c8ee7d42e627f3529fac7fa628352a3
KCVO1995 <943514589@qq.com>
2020-02-15 21:49:34 +0800

    Tags 组件功能基本实现

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)"
@@ -25,9 +25,18 @@
       if (index >= 0) {
         this.selectedTags.splice(index, 1);
       } else {
-        this.selectedTags.push(tag)
+        this.selectedTags.push(tag);
       }
-      console.log(this.selectedTags);
+    }
+
+    create() {
+      const name = window.prompt('请输入标签名');
+      if (this.dataSource && name) {
+        if (this.dataSource.indexOf(name) === -1) {
+          this.$emit('update:dataSource', [...this.dataSource, name])
+        }
+      }
+
     }
 
   }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    <Tags :data-source="tags"/>
+    <Tags :data-source.sync="tags"/>
     <Notes/>
     <Types/>
     <NumberPad/>
