4e56ef938cc5d8c0842f61ecfe173804869af5e3
KCVO1995 <943514589@qq.com>
2020-02-15 17:54:21 +0800

    完成Notes 组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,13 +1,17 @@
 <template>
   <label class="notes">
     <span class="name">备注</span>
-    <input type="text" placeholder="在这里添加备注">
+    <input type="text" :value="value" @input="value = $event.target.value" placeholder="在这里添加备注">
   </label>
 </template>
 <script lang='ts'>
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue {
+    value = '';
+  }
 </script>
 
 <style lang='scss' scoped>
@@ -20,7 +24,6 @@
 
     .name {
       padding-right: 16px;
-
     }
 
     input {
