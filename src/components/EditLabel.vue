de23f189fd937f6be90ac864e3d807a3773f0207
KCVO1995 <943514589@qq.com>
2020-02-17 20:40:47 +0800

    新增 EditLabel 组件

---
+++ src/components/EditLabel.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <div>
+      <Icon name="left"/>
+      <span>编辑标签</span>
+    </div>
+    <label>
+      <span>标签名</span>
+      <input type="text">
+    </label>
+    <div>
+      <button>删除标签</button>
+    </div>
+  </Layout>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import tagListModel from '@/models/tagListModel';
+
+  @Component
+  export default class EditLabel extends Vue {
+    created() {
+      console.log(this.$route.params);
+      const id = this.$route.params.id;
+      tagListModel.fetch();
+      const tags = tagListModel.data;
+      const tag = tags.filter(tag => tag.id === id)[0];
+      if (tag) {
+        console.log(tag);
+      } else {
+        this.$router.replace('/404')
+      }
+    }
+
+  }
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
