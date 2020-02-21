5bc28a951c8974f069e7bda49ea4edda656b0f06
KCVO1995 <943514589@qq.com>
2020-02-21 22:53:40 +0800

    电脑端访问，指引到手机

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,21 @@ new Vue({
   // 原型上
   render: h => h(App)
 }).$mount('#app');
+
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请使用手机打开页面，以保证浏览效果');
+  const img = document.createElement('img');
+  const layout = document.querySelector('.layout-wrapper') as HTMLDivElement;
+  img.src = '/qrcode.png';
+  img.style.position = 'absolute';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%, -100%)';
+  img.style.zIndex = '1';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  if (layout) {
+    layout.style.filter = 'blur(3px)';
+  }
+  document.body.appendChild(img);
+}
