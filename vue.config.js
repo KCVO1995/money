ef3ad24a3fce9aeda64d0681203098b745a0d55f
KCVO1995 <943514589@qq.com>
2020-02-21 22:00:36 +0800

    部署网站到 money-website

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,19 @@ const path = require('path')
 
 module.exports = {
   lintOnSave: false,
-  chainWebpack: config =>{
-    const dir = path.resolve(__dirname, 'src/assets/icons')
+  publicPath: process.env.NODE_ENV === "production"
+    ? "/money-website/"
+    : "/",
+  chainWebpack: config => {
+    const dir = path.resolve(__dirname, "src/assets/icons")
 
     config.module
-      .rule('svg-sprite')
+      .rule("svg-sprite")
       .test(/\.svg$/)
       .include.add(dir).end() // 包含 icons 目录
-      .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract:false}).end()
-      .use('svgo-loader').loader('svgo-loader')
-      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+      .use("svg-sprite-loader-mod").loader("svg-sprite-loader-mod").options({extract: false}).end()
+      .use("svgo-loader").loader("svgo-loader")
+      .tap(options => ({...options, plugins: [{removeAttrs: {attrs: "fill"}}]})).end()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
