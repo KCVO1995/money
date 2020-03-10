4d3ae3c095874076ae277cf15a70bbc5ab9a384d
KCVO1995 <943514589@qq.com>
2020-03-10 14:40:39 +0800

    更新iconList

--- src/views/CreateLabel.vue
+++ src/views/CreateLabel.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    xx
+    <Top leftIcon="left" rightIcon="setting">新增标签</Top>
+    <ul class="current">
+      <li v-for="tag in tagList" :key="tag.id" @click="toggle(tag)"
+          :class="select(tag)">
+        <Icon :name="`${tag.name}`" class="iconTag" v-if="showIcon(tag.name)"/>
+        <div v-else class="firstName">{{firstName(tag.name)}}</div>
+        {{tag.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
