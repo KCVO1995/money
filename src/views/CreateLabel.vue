cea13be4b5642fa1895f98c315fd42e74cd87dbd
KCVO1995 <943514589@qq.com>
2020-03-10 15:04:48 +0800

    CreateLabel 样式基本完成

--- src/views/CreateLabel.vue
+++ src/views/CreateLabel.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <Top leftIcon="left" rightIcon="setting">新增标签</Top>
-    <ul class="current">
-      <li v-for="tag in tagList" :key="tag.id" @click="toggle(tag)"
-          :class="select(tag)">
-        <Icon :name="`${tag.name}`" class="iconTag" v-if="showIcon(tag.name)"/>
-        <div v-else class="firstName">{{firstName(tag.name)}}</div>
-        {{tag.name}}
+    <ul>
+      <li v-for="(newTag, index) in newTagList" :key="index">
+        <Icon :name="`${newTag}`" class="iconTag"/>
+        {{newTag}}
+      </li>
+      <li>
+        <Icon name="add" class="iconTag"/>
+        自定义
       </li>
     </ul>
   </div>
@@ -14,15 +16,36 @@
 
 <script lang='ts'>
   import Vue from 'vue';
+  import iconList from '@/constants/iconList';
   import {Component} from 'vue-property-decorator';
 
   @Component
   export default class CreateTag extends Vue {
-
+    newTagList = iconList;
   }
 
 </script>
 
 <style lang='scss' scoped>
-
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    text-align: center;
+    justify-content: center;
+    color: #999;
+    > li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100px;
+      min-width: 75px;
+      > .iconTag {
+        fill: #999;
+        width: 30px;
+        height: 30px;
+        margin-bottom: 5px;
+      }
+    }
+  }
 </style>
