d6f1563f344911fe9319e3ee40573364a04c026b
KCVO1995 <943514589@qq.com>
2020-03-11 16:27:44 +0800

    将Balls 抽取为组件

---
+++ src/components/Home/Balls.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="main">
+    <div class="type" @click="toRecord('+')">+</div>
+    <div class="output">
+      <div class="income">
+        <span>{{selectedMonth}}月收入</span>
+        <div class="amount">￥ {{show("+")}}</div>
+      </div>
+      <div class="line"></div>
+      <div class="outlay">
+        <div class="amount">￥{{show("-")}}</div>
+        <span>{{selectedMonth}}月支出</span>
+      </div>
+    </div>
+    <div class="type" @click="toRecord('-')">-</div>
+  </div>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component, Inject, Prop} from 'vue-property-decorator';
+  import showAmount from '@/lib/ShowAmount';
+
+  @Component
+  export default class Balls extends Vue {
+    @Inject() eventBus: any;
+    @Prop(Number) selectedMonth: number | undefined;
+
+    toRecord(type: string) {
+      this.$router.push('/money/0');
+      this.$nextTick(() => {
+        this.eventBus.$emit('update:type', type);
+      });
+    }
+
+    show(type: string) {
+      return showAmount(type, 'month', this.selectedMonth - 1);
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  @import "~@/assets/style/helper.scss";
+  .main {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-60%);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+    > .output,
+    .type {
+      @extend %Shadow;
+      border-radius: 50%;
+      background: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    > .output {
+      height: 150px;
+      width: 150px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      > .line {
+        width: 120px;
+        height: 1px;
+        background: #999999;
+        top: 50%;
+        transform: translateY(-50%)
+      }
+      > .income,
+      .outlay {
+        flex-grow: 1;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        > .amount {font-size: 20px;line-height: 35px;
+        }
+        > span {font-size: 12px;line-height: 18px;
+        }
+      }
+      > .income {
+        justify-content: flex-end;
+        > .amount {color: #1296DB;}
+      }
+      > .outlay {
+        > .amount {color: rgba(225, 90, 130)}
+      }
+    }
+    > .type {height: 50px;width: 50px;font-size: 30px;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,38 +4,22 @@
     <ul class="month">
       <li v-for="(mon, index) in months" :key="index" @click="selectMonth(index)">{{mon}}月</li>
     </ul>
-    <div class="main">
-      <div class="type" @click="toRecord('+')">+</div>
-      <div class="output">
-        <div class="income">
-          <span>{{selectedMonth}}月收入</span>
-          <div class="amount">￥ {{show("-")}}</div>
-        </div>
-        <div class="line"></div>
-        <div class="outlay">
-          <div class="amount">￥{{show("+")}}</div>
-          <span>{{selectedMonth}}月支出</span>
-        </div>
-      </div>
-      <div class="type" @click="toRecord('-')">-</div>
-    </div>
+    <Balls :selected-month="selectedMonth">placeholder</Balls>
     <DailyAmount/>
   </div>
 </template>
 
 <script lang='ts'>
   import Vue from 'vue';
-  import {Component, Inject} from 'vue-property-decorator';
-  import DailyAmount from '@/components/DailyAmount.vue';
+  import {Component} from 'vue-property-decorator';
+  import DailyAmount from '@/components/Home/DailyAmount.vue';
   import dayjs from 'dayjs';
-  import showAmount from '@/lib/ShowAmount';
+  import Balls from '@/components/Home/Balls.vue';
 
   @Component({
-    components: {DailyAmount}
+    components: {Balls, DailyAmount}
   })
   export default class Home extends Vue {
-    @Inject() eventBus: any;
-
     selectedMonth = 3;
 
     get months() {
@@ -49,16 +33,7 @@
       return months;
     }
 
-    mounted() {
-      this.selectMonth(1);
-    }
-
-    toRecord(type: string) {
-      this.$router.push('/money/0');
-      this.$nextTick(() => {
-        this.eventBus.$emit('update:type', type);
-      });
-    }
+    mounted() {this.selectMonth(1);}
 
     selectMonth(index: number) {
       this.selectedMonth = this.months[index];
@@ -71,12 +46,7 @@
         }
       }
     }
-
-    show(type: string) {
-      return showAmount(type, 'month', this.selectedMonth - 1);
-    }
   }
-
 </script>
 
 <style lang='scss' scoped>
@@ -104,60 +74,5 @@
         }
       }
     }
-    .main {
-      position: absolute;
-      top: 50%;
-      transform: translateY(-60%);
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      width: 100vw;
-      > .output,
-      .type {
-        @extend %Shadow;
-        border-radius: 50%;
-        background: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      > .output {
-        height: 150px;
-        width: 150px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        > .line {
-          width: 120px;
-          height: 1px;
-          background: #999999;
-          top: 50%;
-          transform: translateY(-50%)
-        }
-        > .income,
-        .outlay {
-          flex-grow: 1;
-          text-align: center;
-          display: flex;
-          flex-direction: column;
-          > .amount {font-size: 20px;line-height: 35px;
-          }
-          > span {font-size: 12px;line-height: 18px;
-          }
-        }
-        > .income {
-          justify-content: flex-end;
-          > .amount {color: #1296DB;}
-        }
-        > .outlay {
-          > .amount {color: rgba(225, 90, 130)}
-        }
-      }
-      > .type {height: 50px;width: 50px;font-size: 30px;
-      }
-    }
   }
-
 </style>
