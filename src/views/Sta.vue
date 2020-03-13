f67973d013bb9fdd8bb214a17dca483a457ca6cb
KCVO1995 <943514589@qq.com>
2020-03-13 18:02:51 +0800

    可以进行更改，但是更改后tag会消失，要重新刷新才有

--- src/views/Sta.vue
+++ src/views/Sta.vue
@@ -4,20 +4,27 @@
     <SelectMonth :selected-month.sync="selectedMonth">placeholder</SelectMonth>
     <SelectDate :selected-date.sync="selectedDate" :selected-month="selectedMonth">placeholder</SelectDate>
     <ShowWeekDaily :selected-date="selectedDate"/>
+    <span class="note">今天明细</span>
     <ul class="record-list">
-      <li v-for="(record,index) in todayRecord('+')" :key="index" class="record">
+      <li v-for="record in todayRecord('+')" :key="record.id" class="record">
         <TagIcon :tagName="record.selectedTags[0].name" class="icon-tag"/>
         <div class="detail">
           <span class="record-amount income">￥{{record.amount}}</span>
           <span class="record-tag">{{record.selectedTags[0].name}}收入</span>
         </div>
+        <router-link :to="`money/${record.id}`">
+          <Icon name="input" class="edit"/>
+        </router-link>
       </li>
-      <li v-for="(record,index) in todayRecord('-')" :key="index" class="record">
+      <li v-for="record in todayRecord('-')" :key="record.id" class="record">
         <TagIcon :tagName="record.selectedTags[0].name" class="icon-tag"/>
         <div class="detail">
           <span class="record-amount outlay">￥{{record.amount}}</span>
           <span class="record-tag">{{record.selectedTags[0].name}}支出</span>
         </div>
+        <router-link :to="`money/${record.id}`">
+          <Icon name="input" class="edit"/>
+        </router-link>
       </li>
     </ul>
   </div>
@@ -37,12 +44,15 @@
   export default class Sta extends Vue {
     selectedMonth = 0;
 
-    selectedDate = dayjs().set('month', this.selectedMonth - 1).set('date', 13);
+    selectedDate = dayjs().set('month', this.selectedMonth - 1).set('date', 1);
 
 
-    mounted() {this.selectedDate = dayjs().set('month', this.selectedMonth - 1).set('date', 13);}
+    mounted() {this.selectedDate = dayjs().set('month', this.selectedMonth - 1).set('date', 1);}
 
-    todayRecord(type: string) {return ShowAmount(type, 'day', this.selectedDate, true);}
+    todayRecord(type: string) {
+      console.log(ShowAmount(type, 'day', this.selectedDate, true));
+      return ShowAmount(type, 'day', this.selectedDate, true);
+    }
 
   }
 </script>
@@ -52,6 +62,14 @@
   $color: #999;
   .global {
     color: $color;
+    > .note {
+      display: block;
+      width: 100%;
+      text-align: center;
+      font-size: 14px;
+      line-height: 14px;
+      margin-top: 25px;
+    }
     > .record-list {
       margin: 0 25px;
       color: $color;
@@ -64,6 +82,14 @@
         margin-top: 20px;
         border-radius: 15px;
         > .icon-tag {margin: auto 20px;}
+        > a {
+          > .edit {
+            width: 20px;
+            height: 20px;
+            margin-right: 15px;
+          }
+        }
+        &:last-child {margin-bottom: 20px;}
         > .detail {
           padding-left: 20px;
           border-left: 1px solid $color;
